67c796c0a9b707caafff6e13adc0490ef46aa8c2
Abiha Fatima <70844041+AbihaFatima@users.noreply.github.com>
2021-09-05 00:46:26 +0400

    Time: 4 ms (89.03%), Space: 8.9 MB (60.47%) - LeetHub

---
+++ move-zeroes/move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    int last = 0, current = 0;
+    while(current < nums.size()) {
+        if(nums[current] != 0) {
+            swap(nums[last], nums[current]);
+            last++;
+        }
+        current++;
+        }
+    }
+};
