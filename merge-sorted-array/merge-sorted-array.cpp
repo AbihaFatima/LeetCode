8d9cf2c73bf6c0163ea1be2cb689f2c1ed3f012c
Abiha Fatima <70844041+AbihaFatima@users.noreply.github.com>
2021-09-06 10:28:17 +0400

    Time: 0 ms (100.00%), Space: 8.9 MB (99.58%) - LeetHub

---
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //Reverse Sorting Approach
+        //We initialize k=m+n-1 as that will be the last location of nums1.
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        
+        while(i>=0&&j>=0)
+        {
+            if(nums1[i]>nums2[j])//keep checking for the greater element of the two arrays(i=m-1,j=n-1) and insert the values.
+            {
+                nums1[k]=nums1[i];
+                i--;
+                k--;
+            }
+            else
+            {
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        }
+        while(i>=0)
+            nums1[k--]=nums1[i--];
+        while(j>=0)
+            nums1[k--]=nums2[j--];
+        
+    }
+};
