e9f47c3a2793e9f584bfb61d02ca288a8f3e88b3
Abiha Fatima <abiha.fatima1908@gmail.com>
2021-10-31 23:55:13 +0400

    Time: 16 ms (85.89%), Space: 19.6 MB (51.87%) - LeetHub

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    //Using Moore Voting Algorithm
+    //Time complexity is O(n)
     int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> m;
         
-    int majority = 0;
-    for (int count = 1, i = 1; i < nums.size(); i++) {
-        nums[majority] == nums[i] ? count++ : count--;
-        if (count == 0) {
-            majority = i;
-            count = 1;
+        //For incrementing the frequency of each element
+        for(auto a:nums){
+            m[a]++;
+            if(m[a]>nums.size()/2)
+                return a;
         }
+        return -1;
     }
+//     //Using Moore Voting Algorithm
+//     int majorityElement(vector<int>& nums) {
         
-    return nums[majority];
-    }
+//     int majority = 0;
+//     for (int count = 1, i = 1; i < nums.size(); i++) {
+//         nums[majority] == nums[i] ? count++ : count--;
+//         if (count == 0) {
+//             majority = i;
+//             count = 1;
+//         }
+//     }
+        
+//     return nums[majority];
+//     }
 };
