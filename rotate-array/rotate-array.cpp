5351cfa9a553dc91ac180548e3e2518130a8722a
Abiha Fatima <abiha.fatima1908@gmail.com>
2021-11-10 16:57:15 +0400

    Time: 32 ms (53.51%), Space: 25 MB (76.73%) - LeetHub

---
+++ rotate-array/rotate-array.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    /*
+    First Approach(brute force approach) - TimeComplexity O(n^2) SpaceComplexity O(1)
+                    Nested Loop each element shift
+    Second Approach(TC and SC trade-off) - TC-O(N) SC-O(2N) ~ O(N)
+                    Auxilary array for storing final rotated array
+    */
+    //Most Optimised Approach TimeComplexity - O(N) && SpaceComplexity-O(1)
+    /*
+    2-Pinter Approach
+    In this Approach we are first rotating rightmost k elements and then rotatating all elements except right most k elements and finally rotating all the elements of the array.
+    */
+    
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1) return; //Base Case
+        
+        k = k%n; //For handling the given constraint (example if value of k is 10^5 and n is 10^3 so to make k equivalent to vector length
+        
+        rotate_aux(nums, n-1-k+1, n-1); //Rotataing k right most elements
+        rotate_aux(nums, 0 ,n-1-k); //Rotating all elements before k right most elements
+        rotate_aux(nums, 0, n-1); //Rotating all the elements of the array
+    }
+    void rotate_aux(vector<int>&nums, int start, int end){
+        int i=start, j=end;
+        while(i<=j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
