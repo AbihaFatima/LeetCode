dc4a93081dd8c19d1805a920d0b723a219af104c
Abiha Fatima <abiha.fatima1908@gmail.com>
2021-11-11 08:32:16 +0400

    Update search-in-rotated-sorted-array.cpp

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    /* Approach : this can be done with a single binary search method as well. Let’s discuss that ->Instead of checking the mid with target (as done in a generic binary search), we check the mid with start and end -> cause the array is distorted -> so first we want to condition on that. Let’s say the nums[start] is less than the nums[mid] -> we get our new start and end -> the start and
-mid.
-So we just add one more condition to the already existing binary search conditions. We shift the start and end pointers after we’ve discovered the subarray where we need to shift. TimeComplexity O(N)
+    /* Approach : this can be done with a single binary search method as well. 
+    Let’s discuss that ->Instead of checking the mid with target (as done in a generic binary search), 
+    we check the mid with start and end -> cause the array is distorted -> so first we want to condition on that. 
+    Let’s say the nums[start] is less than the nums[mid] -> we get our new start and end -> the start and mid.
+So we just add one more condition to the already existing binary search conditions. 
+We shift the start and end pointers after we’ve discovered the subarray where we need to shift. TimeComplexity O(N)
 Here’s the full code:
-
     */
     int search(vector<int>& nums, int target) {
         
