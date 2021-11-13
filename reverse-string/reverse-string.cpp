acdb00db6ceae99baa267cedf230cf8e2f196a62
Abiha Fatima <abiha.fatima1908@gmail.com>
2021-11-13 14:48:37 +0400

    Time: 28 ms (33.91%), Space: 23.1 MB (76.00%) - LeetHub

--- reverse-string/reverse-string.cpp
+++ reverse-string/reverse-string.cpp
@@ -1,24 +1,39 @@
 class Solution {
-    //Recursive Approach
+
 public:
+    //Iterative Solution:
     void reverseString(vector<char>& s) {
         int start = 0;
         int end = s.size()-1;
-        reverseRecursive(s, start, end);
+        while(start<end){
+            swap(s[start++],s[end--]);
+        }
+
     }
     
-    void reverseRecursive(vector<char>&s, int start, int end){
-        int temp = s[start];
-        if(start>=end){
-            return;
-        }
-        else
-        {
-            temp = s[start];
-            s[start]=s[end];
-            s[end] = temp;
+    
+    
+    
+    
+        //Recursive Approach
+    //     void reverseString(vector<char>& s) {
+    //     int start = 0;
+    //     int end = s.size()-1;
+    //     reverseRecursive(s, start, end);
+    // }
+    
+//     void reverseRecursive(vector<char>&s, int start, int end){
+//         int temp = s[start];
+//         if(start>=end){
+//             return;
+//         }
+//         else
+//         {
+//             temp = s[start];
+//             s[start]=s[end];
+//             s[end] = temp;
             
-            reverseRecursive(s, start+1, end-1);
-        }
-    }
+//             reverseRecursive(s, start+1, end-1);
+//         }
+//     }
 };
