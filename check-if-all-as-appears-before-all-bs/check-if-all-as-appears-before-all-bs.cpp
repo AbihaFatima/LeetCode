e7308e4d4b03fa4a5df27567e1030f3ad93b442c
Abiha <abiha.fatima1908@gmail.com>
2022-01-02 19:23:10 +0400

    Time: 0 ms (100.00%), Space: 6.2 MB (64.71%) - LeetHub

---
+++ check-if-all-as-appears-before-all-bs/check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkString(string s) {
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='b' && s[i+1]=='a')
+                return false;
+        }
+            return true;
+    }
+};
