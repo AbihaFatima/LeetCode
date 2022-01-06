1d1a5c4b4aeb421a9d9d90f262c361a3c1107e19
Abiha <abiha.fatima1908@gmail.com>
2022-01-06 15:59:28 +0400

    Time: 11 ms (90.56%), Space: 26.5 MB (36.48%) - LeetHub

---
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if(inorder.size()!=postorder.size()){
+            return NULL;
+        }
+        //Map for hashing the values of the inorder traversal vector
+        map<int,int> inMap;
+        for(int i=0;i<inorder.size();i++){
+            inMap[inorder[i]] = i;
+        }
+        
+        TreeNode* root = getTree(inorder,0,inorder.size()-1,postorder,0,postorder.size()-1,inMap);
+        
+        return root;
+    }
+    //TimeComplexity : O(N)
+    //SpaceComplexity : O(N)
+    TreeNode* getTree(vector<int>& inorder, int inStart, int inEnd, vector<int>& postorder, int postStart, int postEnd, map<int,int>& inMap){
+        //BaseCase
+        if(postStart>postEnd || inStart>inEnd){
+            return NULL;
+        }
+        TreeNode* root = new TreeNode(postorder[postEnd]);
+        int inRoot = inMap[root->val];
+        int numsLeft = inRoot - inStart;
+        
+        root->left = getTree(inorder,inStart,inRoot-1,postorder,postStart,postStart+numsLeft-1,inMap); 
+        
+        root->right = getTree(inorder,inRoot+1,inEnd,postorder,postStart+numsLeft,postEnd-1,inMap); 
+        return root;
+    }
+};
