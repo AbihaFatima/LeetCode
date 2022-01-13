42472f3a9b6b8a3d400d42294b3d21c552603a40
Abiha <abiha.fatima1908@gmail.com>
2022-01-13 23:24:10 +0400

    Time: 12 ms (55.10%), Space: 7.4 MB (45.11%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    //Sorting Approach takes O(N logN) time
+    //                          O(1) space, but some sorting algorithms do take up some space
+//     bool isAnagram(string s, string t) {
+//         if(s.size() != t.size()) return false;
+//         sort(s.begin(),s.end());
+//         sort(t.begin(),t.end());
+        
+//         return (s==t);
+//     }
+    
+    //Hashing Approach TimeComplexity : O(N)
+    //SpaceComplexity : O(N)
+    bool isAnagram(string s, string t){
+        if(s.size() != t.size()) return false;
+        unordered_map<char, int> HashMap;
+        for(char c : s){
+            HashMap[c]++;
+        }
+        for(char c: t){
+            HashMap[c]--;
+            if(HashMap[c]<0) return false;
+        }
+        return true;
+    }
+};
