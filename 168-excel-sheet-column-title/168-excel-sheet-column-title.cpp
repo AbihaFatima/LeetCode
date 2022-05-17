85cf06afca397fe2d9419722010ddcfd33ca8405
Abiha <abiha.fatima1908@gmail.com>
2022-05-17 23:03:34 +0400

    Time: 4 ms (19.75%), Space: 6.1 MB (44.55%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string convertToTitle(int n) {
         string str="";
-        while(n--){
-            int i = n%26;
-            str+='A'+i;
-            n/=26;
+        while(n--){ //example n=28 -> AB
+            int i = n%26;   //so i=28%26 = 2
+            str+='A'+i;     //'A'+2 => B 
+            n/=26;          //n=28/26 = 1 now again loop will execute; i=1%26=0 => 'A'+0=>A
         }
-        reverse(str.begin(),str.end());
+        reverse(str.begin(),str.end());// str=BA after reversing AB
         return str;
     }
 };
