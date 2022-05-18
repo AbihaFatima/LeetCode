504ac31524dc7e9b4232d07d5c08f6d34b47294a
Abiha <abiha.fatima1908@gmail.com>
2022-05-18 23:09:50 +0400

    Time: 2 ms (45.24%), Space: 5.8 MB (85.30%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.cpp
@@ -3,14 +3,27 @@ public:
     //Brute-force solution
     //Time Complexity: O(N)
     //Space Complexity: O(N)
+    // int kthFactor(int n, int k) {
+    //     if(k==1)    return 1;
+    //     vector<int>factors;
+    //     for(int i=1;i<=n;i++){
+    //         if(n%i==0) factors.push_back(i);
+    //     }
+    //     if(k>factors.size()) return -1;
+    //     else
+    //         return factors[k-1];
+    // }
+    
+    //Optimal Solution
+    //TC:O(n/2) and SC:O(1)
     int kthFactor(int n, int k) {
-        if(k==1)    return 1;
-        vector<int>factors;
-        for(int i=1;i<=n;i++){
-            if(n%i==0) factors.push_back(i);
+        int i=1;
+        for(i=1;i<=n/2;i++){
+            if(n%i==0) k--;
+            if(k==0) return i;
         }
-        if(k>factors.size()) return -1;
-        else
-            return factors[k-1];
+        if(k==1) return n; //for prime numbers as only 2 factors are present
+        return -1;
     }
+    
 };
