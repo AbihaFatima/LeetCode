984cea99a13bdf966b70c9ea3a50048881bebee3
Abiha <abiha.fatima1908@gmail.com>
2022-06-03 00:37:16 +0400

    Time: 0 ms (100.00%), Space: 7.1 MB (73.66%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    //Optimised in-place solution
+    //TC:O(N^2)
+    //SC: O(1)
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        //Finding Transpose of the matrix
+        for(int i=0;i<n;i++)
+            for(int j=i;j<n;j++)
+                swap(matrix[i][j],matrix[j][i]);
+        
+        //Swapping the columns by 2-pointer approach to get matrix rotated by 90 degrees
+        for(int i=0;i<n;++i){
+            int left=0, right=n-1;
+            while(left<right){
+                swap(matrix[i][left],matrix[i][right]);
+                ++left;
+                --right;
+            }
+        }
+    }
+};
