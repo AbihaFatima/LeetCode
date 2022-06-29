7cc7694143def1f06a21e1c53f0ce22b3a518f6b
Abiha <abiha.fatima1908@gmail.com>
2022-06-29 23:54:18 +0400

    Time: 0 ms (100.00%), Space: 12 MB (76.41%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -18,17 +18,25 @@ public:
         if(root==NULL){
             return ans;
         }
+        
         queue<TreeNode*> TreeQueue;
         TreeQueue.push(root);
+        
         bool leftToRight = true;
+        
         while(!TreeQueue.empty()){
+            
             int size = TreeQueue.size();
             vector<int>row(size);
+            
             for(int i=0;i<size;i++){
                 TreeNode* node = TreeQueue.front();
                 TreeQueue.pop();
-                int index = (leftToRight) ? i : (size-1-i);
+                
+                int index = (leftToRight) ? i : (size-1-i);//if leftToRight is true push nodes left to right normally as in level order traversal
+                //else set the index to last i.e pushing it from right to left
                 row[index] = node->val;
+                
                 if(node->left){
                     TreeQueue.push(node->left);
                 }
@@ -36,8 +44,8 @@ public:
                     TreeQueue.push(node->right);
                 }
             }
-                leftToRight = !leftToRight;
-                ans.push_back(row);
+            leftToRight = !leftToRight;
+            ans.push_back(row);
         }
         return ans;
     }
