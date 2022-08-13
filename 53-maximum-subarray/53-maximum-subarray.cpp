649dcb53957aba957903f0ac7e909e48dcddce4e
Abiha <abiha.fatima1908@gmail.com>
2022-08-13 22:32:07 +0530

    Time: 123 ms (89.92%), Space: 67.8 MB (54.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-       int max_so_far = nums[0];
-       int curr_max = nums[0];
+    //Brute-force approach  - O(N^2)
+//     int maxSubArray(vector<int>& nums) {
+//        int max_so_far = nums[0];
+//        int curr_max = nums[0];
 
-       for (int i = 1; i < nums.size(); i++)
-       {
-            curr_max = max(nums[i], curr_max+nums[i]);
-            max_so_far = max(max_so_far, curr_max);
-       }
-       return max_so_far;
+//        for (int i = 1; i < nums.size(); i++)
+//        {
+//             curr_max = max(nums[i], curr_max+nums[i]);
+//             max_so_far = max(max_so_far, curr_max);
+//        }
+//        return max_so_far;
+//     }
+    
+    //Optimised Approach:
+    int maxSubArray(vector<int>& nums){
+        int maxSum = INT_MIN;
+        int currSum = 0;
+        for(int i=0;i<nums.size();i++){
+            currSum += nums[i];
+            if(maxSum < currSum){
+                maxSum = currSum;
+            }
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
     }
 };
