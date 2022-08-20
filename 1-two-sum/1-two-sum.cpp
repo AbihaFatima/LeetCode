74948739c7f3e1a438243409512b57d1d3bb07fe
Abiha <abiha.fatima1908@gmail.com>
2022-08-20 07:53:05 +0530

    Time: 17 ms (73.65%), Space: 10.9 MB (47.75%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //Optimised Solution O(n)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        vector<int> result;
+            for (int i = 0; i < nums.size(); i++) {
+            if (hash.count(target - nums[i])) // If the pair of this value to reach the target sum has been saved already
+            {
+                result.push_back(hash[target - nums[i]]); // Get index of other value
+                result.push_back(i);
+                return result;
+            }
+            else // Pair has not yet been found, so save the value to hash table
+                hash[nums[i]] = i;
+            }
+        return result;
+    }
+};
