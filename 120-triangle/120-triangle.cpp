5a7a5982e4698282d89b9aaec917d2dcb82359a7
Abiha <abiha.fatima1908@gmail.com>
2022-08-21 23:37:44 +0530

    Time: 10 ms (54.76%), Space: 8.5 MB (92.89%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     //DP Approach
     //Space Complexity: O(1)
-    //TC: O(N^2) \U0001f914maybe
+    //TC: O(N^2) 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         for(int i=n-2;i>=0;i--){
