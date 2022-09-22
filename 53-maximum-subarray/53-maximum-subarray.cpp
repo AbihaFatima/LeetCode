325b4fdff498d84ce6ca584fbf41d0c81c255d6e
Abiha <abiha.fatima1908@gmail.com>
2022-09-22 17:17:10 +0530

    Time: 259 ms (14.05%), Space: 67.8 MB (10.75%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -16,17 +16,29 @@ public:
     //Optimised Approach:
     //Time Complexity : O(N)
     //Kadane's Algorithm
+    // int maxSubArray(vector<int>& nums){
+    //     int maxSum = INT_MIN;
+    //     int currSum = 0;
+    //     for(int i=0;i<nums.size();i++){
+    //         currSum += nums[i];
+    //         if(maxSum < currSum){
+    //             maxSum = currSum;
+    //         }
+    //         if(currSum < 0){
+    //             currSum = 0;
+    //         }
+    //     }
+    //     return maxSum;
+    // }
+    
     int maxSubArray(vector<int>& nums){
-        int maxSum = INT_MIN;
-        int currSum = 0;
-        for(int i=0;i<nums.size();i++){
+        int currSum = 0, maxSum=INT_MIN;
+        int i = 0, n = nums.size();
+        while(i < n){
             currSum += nums[i];
-            if(maxSum < currSum){
-                maxSum = currSum;
-            }
-            if(currSum < 0){
-                currSum = 0;
-            }
+            maxSum = max(currSum, maxSum);
+            if(currSum < 0) currSum=0;
+            i++;
         }
         return maxSum;
     }
