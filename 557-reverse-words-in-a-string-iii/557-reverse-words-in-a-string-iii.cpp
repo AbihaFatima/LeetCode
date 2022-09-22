e2c172e4bcc7ccd4953048869782c219b1f28174
Abiha <abiha.fatima1908@gmail.com>
2022-09-22 17:31:40 +0530

    Time: 37 ms (24.50%), Space: 9.8 MB (45.42%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    //TimeComplexity : O(N)
+    //SpaceComplexity : O(1)
+    string reverseWords(string s) {
+        int i=0;
+        for(int j=0;j<s.size();j++){
+            if(s[j]==' '){
+                reverse(s.begin()+i,s.begin()+j);
+                i = j+1;
+            }
+        }
+        reverse(s.begin() + i, s.end());
+        return s;
+    }
+};
