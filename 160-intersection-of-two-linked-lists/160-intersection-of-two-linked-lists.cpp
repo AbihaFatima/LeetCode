79982a5413f3fb7f32b877df8ca2de33ae82569a
Abiha <abiha.fatima1908@gmail.com>
2022-09-24 13:38:22 +0530

    Time: 46 ms (95.75%), Space: 14.6 MB (73.29%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -10,23 +10,61 @@ class Solution {
 public:
     //Brute Force Approach: traversing and checking nodes
     
-    
+    //************************ HASHING **********************
     //Hashing can also be done : by storing  the address and checking the address in the hash table and returning if the address is repeated in the hashtable
     //TC: O(M+N)
     //SC: O(M)
     
-    ListNode *getIntersectionNode(ListNode *A, ListNode *B){
-        unordered_map<ListNode*, int>mp;
+    // ListNode *getIntersectionNode(ListNode *A, ListNode *B){
+    //     unordered_map<ListNode*, int>mp;
+    //     while(A!=NULL){
+    //         mp[A]++;
+    //         A=A->next;
+    //     }
+    //     while(B!=NULL){
+    //         if(mp[B] > 0) return B;
+    //         B=B->next;
+    //     }
+    //     return NULL;
+    // }
+    
+//****************** LENGTH DIFFERENCE APPROACH *****************************
+//TC: O(m+n)
+//SC: O(1)
+    
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB){
+        ListNode *A = headA;
+        ListNode *B = headB;
+        int l1=0,l2=0;
         while(A!=NULL){
-            mp[A]++;
-            A=A->next;
+            l1++;
+            A = A->next;
         }
         while(B!=NULL){
-            if(mp[B] > 0) return B;
-            B=B->next;
+            l2++;
+            B = B->next;
+        }
+        int diff = abs(l2-l1);
+        if(l1>l2){
+            while(diff){
+                headA = headA->next;
+                diff--;
+            }
+        }
+        else{
+            while(diff){
+                headB = headB->next;
+                diff--;
+            }
+        }
+        while(headA!=NULL and headB!=NULL){
+            if(headA == headB) return headA;
+            headA = headA->next;
+            headB = headB->next;
         }
         return NULL;
     }
+    
     
     
     //Time Complexity O(2M) M is length of longer linked list
