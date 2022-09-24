7f14d70e8a5dcd9eb8dce248c6adc4f9c622f7f0
Abiha <abiha.fatima1908@gmail.com>
2022-09-24 13:08:24 +0530

    Time: 71 ms (62.50%), Space: 21.1 MB (5.32%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,20 +9,39 @@
 class Solution {
 public:
     //Brute Force Approach: traversing and checking nodes
+    
+    
     //Hashing can also be done : by storing  the address and checking the address in the hash table and returning if the address is repeated in the hashtable
+    //TC: O(M+N)
+    //SC: O(M)
+    
+    ListNode *getIntersectionNode(ListNode *A, ListNode *B){
+        unordered_map<ListNode*, int>mp;
+        while(A!=NULL){
+            mp[A]++;
+            A=A->next;
+        }
+        while(B!=NULL){
+            if(mp[B] > 0) return B;
+            B=B->next;
+        }
+        return NULL;
+    }
+    
+    
     //Time Complexity O(2M) M is length of longer linked list
     //Space Complecity is O(1)
     
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode *a = headA;
-        ListNode *b = headB;
-        //If a and b have different length we will stop the iteration after 2nd iteration
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode *a = headA;
+//         ListNode *b = headB;
+//         //If a and b have different length we will stop the iteration after 2nd iteration
         
-        while(a!=b){
-            //For the first iteration we will reset the pointer to the head of another linked list
-            a = a==NULL ? headB : a->next;
-            b = b==NULL ? headA : b->next;
-        }
-        return a;
-    }
+//         while(a!=b){
+//             //For the first iteration we will reset the pointer to the head of another linked list
+//             a = a==NULL ? headB : a->next;
+//             b = b==NULL ? headA : b->next;
+//         }
+//         return a;
+//     }
 };
