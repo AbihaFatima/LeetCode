66f8ace4b1e44ccc5f771925f4b14cbf544f4866
Abiha <abiha.fatima1908@gmail.com>
2022-09-27 16:58:43 +0530

    Time: 18 ms (39.08%), Space: 7.6 MB (37.57%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -10,40 +10,20 @@ class Solution {
 public:
     //Time Complexity is O(N)
     //Space complexity is O(1)
-//     ListNode *detectCycle(ListNode *head) {
-//         if (head == NULL || head->next == NULL)
-//             return NULL;
-//         ListNode *slow = head;
-//         ListNode *fast = head;
-//         ListNode *temp = head;
-        
-//         //Find the collision point
-//         while(fast->next != NULL && fast->next->next != NULL){
-//             slow = slow->next;
-//             fast = fast->next->next;
-//             if(slow==fast){ //Cycle exists
-//                 while(slow != temp){ //entry(temp) location found
-//                     slow = slow->next;
-//                     temp = temp->next;
-//                 }
-//                 return temp;
-//             }
-//         }
-//         return NULL;
-//     }
+    
     ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL)
+            return NULL;
         ListNode *slow = head;
         ListNode *fast = head;
         ListNode *temp = head;
         
-        //Find Collision point
-        while(fast!=NULL && fast->next!=NULL){
+        //Find the collision point
+        while(fast->next != NULL && fast->next->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-            if(slow==fast){
-                //LL cycle exists
-                //Finding the collision location
-                while(slow!=temp){
+            if(slow==fast){ //Cycle exists
+                while(slow != temp){ //entry(temp) location found
                     slow = slow->next;
                     temp = temp->next;
                 }
@@ -52,4 +32,5 @@ public:
         }
         return NULL;
     }
+
 };
