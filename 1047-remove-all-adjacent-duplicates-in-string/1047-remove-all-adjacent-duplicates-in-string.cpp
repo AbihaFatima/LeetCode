ec31cc7d701beb7997b1b5c7dba84f7f928df33d
Abiha <abiha.fatima1908@gmail.com>
2022-10-05 10:36:39 +0530

    Time: 1190 ms (5.96%), Space: 780.2 MB (7.47%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    //TC : O(N)
-    //SC : O(N)
     string removeDuplicates(string s) {
-    
-    stack<char> st; // Store the string without duplicate elements
-     
-    int i = 0;// Store the index of str
-     
-    // Traverse the string s
-    while (i < s.length())
-    {
-        if (st.empty() || s[i] != st.top())
-        // Checks if stack is empty or top of the stack is not equal to current character
-        {
-            st.push(s[i]);
-            i++;
+        int n = s.length();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(st.empty() || st.top()!=s[i]){
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+            }
         }
-        else
-        // If top element of the stack is equal to the current character
-        {
-            st.pop();
-            i++;
+        string ans="";
+        if(!st.empty()){
+            while(!st.empty()){
+                ans = st.top() + ans;
+                st.pop();
+            }
         }
-    }
-      
-    // If stack is not Empty
-    string short_string = "";
-    if(!st.empty())
-    {
-        while (!st.empty())
-        {
-            short_string = st.top() +
-                           short_string;
-            st.pop();
-        }
-    }
-    return short_string;
+        return ans;
     }
 };
