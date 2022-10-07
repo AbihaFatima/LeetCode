457782752179d85e915495bdd19e0b8bb7e2cdc6
Abiha <abiha.fatima1908@gmail.com>
2022-10-07 23:33:12 +0530

    Time: 247 ms (34.17%), Space: 59.6 MB (93.61%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-    /*
-    [0,1,2,1]
-    
-    [0,10,5,2] 
-     ^      ^
-    */
+    //Binary Search Approach
+    //TC: O(logN)
+    //SC: O(1)
     int peakIndexInMountainArray(vector<int>& arr) {
         int start=0,end=arr.size()-1,mid;
         while(start<end){
