dba5ffa7af31fac58509e1d2ff20c633142bbfe6
Abiha <abiha.fatima1908@gmail.com>
2022-10-24 21:18:07 +0530

    Time: 7 ms (68.81%), Space: 7.4 MB (41.47%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>st;
+        st.push(-1);
+        int maxLen=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i] == '(') st.push(i);
+            else{
+                st.pop();
+                if(st.empty()) st.push(i);
+                maxLen = max(maxLen, i-st.top());
+            }
+        }
+        return maxLen;
+    }
+};
