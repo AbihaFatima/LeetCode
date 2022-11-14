f7b6f980066b5925c2312aa9f6a3a33c6039685a
Abiha <abiha.fatima1908@gmail.com>
2022-11-14 19:23:56 +0530

    Time: 136 ms (69.14%), Space: 10.4 MB (91.40%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    //Approach - DP(tabulation method)
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        int t[n+1][amount+1];
+        //Initialization of 0th row and 0th col
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=amount;j++){
+                if(i==0) t[i][j]=INT_MAX-1;
+                if(j==0) t[i][j]=0;
+            }
+        }
+        //Initialization of 1st row and 1st col
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(j%coins[i-1]==0){
+                    t[i][j] = j/coins[i-1];
+                }
+                else{
+                    t[i][j] = -1;
+                }
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1] <= j){
+                    t[i][j] = min(1+t[i][j-coins[i-1]], t[i-1][j]);
+                }
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        }
+        return (t[n][amount] == INT_MAX || t[n][amount] == INT_MAX-1) ? -1 : t[n][amount];
+    }
+};
