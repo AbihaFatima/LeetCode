f88e147be44451ce10602d38b166f3d5a9274ef0
Abiha <abiha.fatima1908@gmail.com>
2023-01-15 01:21:32 +0530

    Time: 39 ms (79.75%), Space: 22.3 MB (76.20%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
-    //using vector of pairs
+    // //using vector of pairs
+    // vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+    //     vector<pair<int,string>>v;
+    //     for(int i=0;i<names.size();i++){
+    //         v.push_back({heights[i],names[i]});
+    //     }
+    //     sort(v.rbegin(),v.rend());
+    //     for(int i=0;i<names.size();i++){
+    //         names[i] = v[i].second;
+    //     }
+    //     return names;
+    // }
+    
+    
+    //Using Max-Heap
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        vector<pair<int,string>>v;
+        priority_queue<pair<int,string>>pq;
         for(int i=0;i<names.size();i++){
-            v.push_back({heights[i],names[i]});
+            pq.push({heights[i],names[i]});
         }
-        sort(v.rbegin(),v.rend());
-        for(int i=0;i<names.size();i++){
-            names[i] = v[i].second;
+        int i=0;
+        while(i <= names.size() && !pq.empty()){
+            names[i] = pq.top().second;
+            pq.pop();
+            i++;
         }
         return names;
     }
