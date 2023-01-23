d6d835265ceb8c751a30d3a973eff03bcce1cdde
Abiha <abiha.fatima1908@gmail.com>
2023-01-23 20:45:31 +0530

    Time: 23 ms, Memory: 16.8 MB - LeetHub

--- single-number/single-number.cpp
+++ single-number/single-number.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) {
-        unordered_map<int,int> hm;
-        for (int i = 0; i < nums.size(); i++){
-            hm[nums[i]]++;
-        }
-        int SNo;
-        for(auto it=hm.begin();it!=hm.end();it++) {
-        if (it->second == 1) {
-            SNo = it->first;
+    //Linear time but not using constant space
+    // int singleNumber(vector<int>& nums) {
+    //     unordered_map<int,int> hm;
+    //     for (int i = 0; i < nums.size(); i++){
+    //         hm[nums[i]]++;
+    //     }
+    //     int SNo;
+    //     for(auto it=hm.begin();it!=hm.end();it++) {
+    //     if (it->second == 1) {
+    //         SNo = it->first;
+    //         }
+    //     }
+    //     return SNo;
+    // }
+    
+    //Linear time and constant space:
+        int singleNumber(vector<int>& nums) {
+            int ans=0;
+            for(auto i:nums){
+                ans ^= i;
             }
+            return ans;
         }
-        return SNo;
-    }
 };
