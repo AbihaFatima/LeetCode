aec419087f635e9d0ac0e00fb1756f3c1096f1f3
Abiha <abiha.fatima1908@gmail.com>
2023-01-25 10:50:21 +0530

    Time: 10 ms (87.40%), Space: 8.4 MB (62.71%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    //Approach:
+    //while iterating mark the position of each character
+    //At ith character, check the previous occurance and decide the start of the substring accordingly
+    //Self explanatory code
+    int lengthOfLongestSubstring(string s) {
+        int max_len=0,start=-1;
+        vector<int>v(256,-1);
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(v[s[i]]>start){
+                start = v[s[i]];
+            }
+            v[s[i]] = i;
+            max_len = max(max_len,i-start);
+        }
+        return max_len;
+    }
+};
