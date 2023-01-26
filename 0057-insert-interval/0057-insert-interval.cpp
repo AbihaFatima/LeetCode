f897eb8d79360df59f2cc8c37550fcd55913427d
Abiha <abiha.fatima1908@gmail.com>
2023-01-27 00:15:45 +0530

    Time: 11 ms (94.44%), Space: 17.2 MB (43.93%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& newIn) {
-        in.push_back(newIn);
-        sort(in.begin(),in.end());
         vector<vector<int>> res;
-        res.push_back(in[0]);
         int j=0;
-        for(int i=1;i<in.size();i++){
-            if(res[j][1] >= in[i][0]){
-                res[j][1] = max(res[j][1],in[i][1]);
-            }
-            else{
-                res.push_back(in[i]);
-                j++;
-            }
+        //non-overlapping intervals before newInterval
+        while(j<in.size() and in[j][1] < newIn[0]) res.push_back(in[j++]);
+        //for overlapping intervals
+        while(j<in.size() and in[j][0] <= newIn[1]){
+            newIn[0] = min(in[j][0],newIn[0]);
+            newIn[1] = max(in[j][1],newIn[1]);
+            j++;
         }
+        res.push_back(newIn);
+        //for intervals after overlapping intervals
+        while(j<in.size()) res.push_back(in[j++]);
         return res;
     }
 };
