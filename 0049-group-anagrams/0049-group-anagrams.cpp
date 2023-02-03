9f9dce5df083777002fd32e63d0cf566f763439b
Abiha <abiha.fatima1908@gmail.com>
2023-02-03 11:41:35 +0530

    Time: 30 ms (94.77%), Space: 19.5 MB (85.66%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    //Time Complexity : NlogN 
+    //Space Complexity : O(N)
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        if(strs.size() == 1){
+            return {{strs[0]}};
+        }
+        vector<vector<string>> res;
+        unordered_map<string, vector<string>> m;
+        for(int i=0;i<strs.size();i++){
+            string s = strs[i];
+            sort(strs[i].begin(),strs[i].end()); //Sorting the string
+            m[strs[i]].push_back(s); //Sorted string is the key and value is initial string
+        }
+        for(auto i = m.begin();i != m.end();i++){
+            res.push_back(i->second);
+        }
+        return res;
+    }
+};
