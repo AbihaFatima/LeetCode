2efad32d0227a00d3fedd0b08c401195ca3cea19
Abiha <abiha.fatima1908@gmail.com>
2023-02-27 11:19:19 +0530

    Time: 5 ms (61.47%), Space: 9.8 MB (32.79%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //Binary Search O(log n) 
+    //Finding floor in sorted array
+    int searchInsert(vector<int>& nums, int target) {
+        if(target > nums[nums.size()-1]) return nums.size();
+        if(target < nums[0]) return 0;
+        int low=0,mid,high=size(nums)-1;
+        while(low <= high)
+        {
+            mid = low + (high-low)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) low = mid + 1;
+            else high = mid-1;
+        }
+        return low;
+    }
+};
