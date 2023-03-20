e883358d903b60bc6743a703ec1b736200ee9769
Abiha <abiha.fatima1908@gmail.com>
2023-03-20 12:14:53 +0400

    Time: 10 ms (47.54%), Space: 8.9 MB (43.59%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        
         unordered_map<int, int> indices;
-        
-        for(int i = 0; i < nums2.size(); i++){
+        for(int i = 0; i < nums2.size(); i++)
             indices[nums2.at(i)] = i;
-        }
-        
         vector<int> greaterElements;
-        
         for(int i = 0; i < nums1.size(); i++){
             int index = indices[nums1.at(i)];
-            
             bool foundGreaterElement = false;
             for(int right = index + 1; right < nums2.size(); right++){
                 if(nums2.at(right) > nums1.at(i)){
@@ -21,11 +15,9 @@ public:
                     break;
                 }
             }
-            
             if(!foundGreaterElement)
                 greaterElements.push_back(-1);
         }
-        
         return greaterElements;
     }
 };
