44b9b31993ebd231ead8fdc3a6b3a3f40439e667
Abiha <abiha.fatima1908@gmail.com>
2023-06-26 22:55:33 +0400

    Time: 194 ms (85.27%), Space: 72.9 MB (61.12%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        long long cost =0;
+        int n = costs.size();
+        priority_queue<int, vector<int> , greater<int>> ft, lt;
+        int i=0, j=n-1;
+        while(k--){
+            while(ft.size() < candidates && i<= j) ft.push(costs[i++]);
+            while(lt.size() < candidates && i<= j) lt.push(costs[j--]);
+            int a = ft.size() ? ft.top() : INT_MAX;
+            int b = lt.size() ? lt.top() : INT_MAX;
+            if(a<=b){
+                cost += a;
+                ft.pop();
+            }
+            else {
+                cost += b;
+                lt.pop();
+            }
+        }
+        return cost;
+    }
+};
