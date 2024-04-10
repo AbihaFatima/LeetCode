73039ea034b212da31b6a1aac90085e04eaa48d0
Abiha <abiha.fatima1908@gmail.com>
2024-04-10 23:51:37 +0400

    Time: 0 ms (100.00%), Space: 8.7 MB (9.68%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,16 +11,11 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        //Two Pointer Approach
-        if(head->next==NULL || head==NULL){
-            return head;
-        }
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
+        ListNode *slow = head;
+        ListNode *fast = head;
         while(fast!=NULL && fast->next!=NULL){
-            fast = fast->next->next;
-            slow = slow->next;
+            slow=slow->next;
+            fast=fast->next->next;
         }
         return slow;
     }
