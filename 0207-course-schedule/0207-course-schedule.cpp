b39f6062f62fc849135ff19716cf120df8a91d14
Abiha <abiha.fatima1908@gmail.com>
2026-01-02 15:49:01 +0400

    Time: 1 ms (84.51%), Space: 19.3 MB (56.03%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,54 +1,42 @@
 class Solution {
-    // bool isCyclic(vector<vector<int>>& adj,vector<int>& visited,int curr){
-    //     if(visited[curr] == 2) return true;
-    //     visited[curr]=2;
-    //     for(int i=0;i<adj[curr].size();++i)
-    //         if(visited[adj[curr][i]]!=1)
-    //             if(isCyclic(adj,visited,adj[curr][i]))
-    //                 return true;
-    //     visited[curr] = 1;
-    //     return false;
-    // }
-        bool isCyclic(vector<vector<int>>& adj,vector<int>& visited,vector<int> &dfsVisited, int curr){
-        //if(visited[curr] == 2) return true;
-        //visited[curr]=2;
-            dfsVisited[curr]=1;
-            visited[curr]=1;
-            for(auto it:adj[curr])
-                {
-                    if(!visited[it])
-                    {
-                        if(isCyclic(adj,visited,dfsVisited,it))
-                        return true;
-                    }
-                    else if(dfsVisited[it]==1) return true;
-                }
-            dfsVisited[curr] = 0;
-            return false;
-    }
-    
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        /*https://www.geeksforgeeks.org/fast-io-for-competitive-programming/*/
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        
-        vector<vector<int>> adj(numCourses);
-        int n = prerequisites.size();
-        //make a directed graph
-        for(int i=0;i<n;i++){
-            adj[prerequisites[i][0]].push_back(prerequisites[i][1]);
+    //tc: o(v+e) and sc: o(v+e)
+    bool dfs(int node, vector<vector<int>>& adjList, vector<int>& state){
+        //this node was already in path (recursion stack) n now we encountered it again so return false as it forms cycle
+        if(state[node] == 1) return false;
+
+        //this node was fully processed in prev path so, doesnt builds cycle
+        if(state[node] == 2) return true;
+
+        //if the node was unvisted i.e 0 make it visited
+        state[node]=1;
+
+        //add its neighbours to recursion stack
+        for(int n : adjList[node]){
+                if(!dfs(n, adjList,state)) return false; //its neighbours in future calls forms cycle
         }
         
-        vector<int>visited(numCourses,0);
-        vector<int>dfsVisited(numCourses,0);
-        
-        for(int i=0;i<numCourses;i++){
-            if(visited[i]==0){
-                if(isCyclic(adj,visited,dfsVisited,i))
-                    return false;
+        //after visiting its neighbours, mark the initial parent node as fully processed
+        state[node] = 2;
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int>state(numCourses, 0);
+        //states that we will use; 0-> unvisted, 1->currently being processed that is middle of some path, 2->fully processed without cycle detection
+
+
+        //building directed adjList from bi->ai
+        vector<vector<int>> adjList(numCourses);
+        for(auto &e : prerequisites){
+            int a = e[0], b=e[1];
+            adjList[b].push_back(a);
+        }
+
+        //Starting from unvisited node doing a dfs traversal
+        for(int i=0; i<numCourses; i++){
+            if(state[i]==0){
+                if(!dfs(i, adjList, state)) return false;
             }
         }
         return true;
