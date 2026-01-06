2461574c7b5666379b557c5f25449450474be360
Abiha <abiha.fatima1908@gmail.com>
2026-01-06 16:10:29 +0400

    Time: 0 ms (100%), Space: 14.5 MB (72.6%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,5 +1,15 @@
 class Solution {
 public:
+/*
+So basically if you look at the rooms vector, it has the nodes that ith node connects to,
+which makes it a directed graph and the rooms vector is the adjacenyList, so we are basically maintaining a visited array of nodes
+and traversing and checking that can we visit the node from that point, at the end we check if there are any unvisited nodes in the
+bool visited array, if so we return false, and if no unvisited nodes that means we are able to open all nodes, hence returning true
+
+TC: O(n + total_keys) ; total_keys = edges so -> O(V+E)
+SC: O(N) + O(N) that is recursion stack and visited node
+*/
+
     void dfs(int node, vector<vector<int>>& rooms, vector<bool>& visited){
         visited[node] = true;
         //check its neighbours
