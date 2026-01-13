9d86ada9d3b49d7b2d5399d992a5c7d7bad22087
Abiha <abiha.fatima1908@gmail.com>
2026-01-13 12:34:12 +0400

    Time: 0 ms (100%), Space: 28.5 MB (64.01%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+//If one value appears n times in an array of size 2n, at least two occurrences being near each other. 
+
+    int repeatedNTimes(vector<int>& nums) {
+        int len = nums.size();
+        for(int i = 0;i<len-1;i++) {
+            if(nums[i] == nums[i+1]) return nums[i];
+            if(i+2 < len && nums[i]==nums[i+2]) return nums[i];
+
+            ////Why up to +3? Because with n repetitions, at least two copies must fall within distance ≤ 3.
+            if(i+3 < len && nums[i]==nums[i+3]) return nums[i];
+            
+        }
+        return -1;
+    }
+};
